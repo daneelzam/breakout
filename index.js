660a8c599d592f77578cd2cb0ab16d017d1646e2
Daniel Zameshaev <daneelzam@yandex.ru>
2021-01-26 13:52:36 +0300

    simple aimation

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-// красный квадрат
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
-// зеленый круг
-ctx.beginPath();
-ctx.arc(240, 160, 30, 0, Math.PI * 10, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
-// пустой прямоугольник с синей обводкой
-ctx.beginPath();
-ctx.rect(160, 10, 100, 50);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+const dx = 2;
+const dy = 0;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  ctx.closePath();
+}
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 100);
